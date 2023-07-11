41a70a72fc82b51187977cb269c5537d5623a8d9
Lucas Martin <lucasmartinmacedo@gmail.com>
2023-07-10 21:48:20 -0300

    Changelog:
    - Adição do stylesheet do bootstrap, bootstrap plus e style no login.php

--- Application_main/Scripts/PHP/includes/login.php
+++ Application_main/Scripts/PHP/includes/login.php
@@ -12,6 +12,21 @@ else
     ?>
 
     <html lang="pt-br">
+
+        <head>
+
+            <!-- region Meta Tags -->
+            <meta charset="UTF-8">
+
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+            <link rel="stylesheet" href="/Application_main/Scripts/CSS/style.css" type="text/css">
+            <link rel="stylesheet" href="/Application_main/Scripts/CSS/boostrap-plus.css" type="text/css">
+            <!-- endregion -->
+
+            <title>CMS Editor</title>
+
+        </head>
+
         <body>
             <div class="container">
                 <a href="/index.php" id="logo"> CMS </a>
