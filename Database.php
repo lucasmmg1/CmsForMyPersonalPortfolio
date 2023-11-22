ba536ad3c02e828ca77793c6d4a515084ee670c5
Lucas Martin de Macedo <lucasmartinmacedo@gmail.com>
2023-11-22 00:22:56 -0300

    Changelog:
    - Correção do uso das variáveis

--- Database.php
+++ Database.php
@@ -14,7 +14,7 @@
 
             if (!isset(Database::$conn))
             {
-                Database::$conn = new connection(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_PORT'), getenv('DB_CHARSET'));
+                Database::$conn = new connection($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_PORT'], $_ENV['DB_CHARSET']);
                 //Database::$conn = new connection("179.209.42.16", "dashboard", "dashboard", "119948b642a772402e9872798a119948b642a772402e9872789a", "3306", "utf8");
             }
 
